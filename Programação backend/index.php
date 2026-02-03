48607658f49eae30c6c6e69f1cd5549bde9c25df
AllanCamargoMartins <camargomartinsallan@gmail.com>
2026-02-03 14:01:18 -0300

    Primeiro commit de 2026

---
+++ Programação backend/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Pagina inicial</title>
+</head>
+<body>
+    <?php
+
+    echo "<h1>Pagina principal do meu servidor</h1>";
+
+?>
+<a href="
+POO/index.php">Aula de POO</a><br>
+</body>
+</html>
